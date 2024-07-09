514fc0b759e4045819ab4dd4e4dd29345be51d96
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-07-09 13:50:46 +0200

    Clear data after sent order

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,6 +1,6 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './OrderForm.module.scss';
-import { getCart } from '../../../redux/cartRedux';
+import { clearCart, getCart } from '../../../redux/cartRedux';
 import { Row } from 'react-bootstrap';
 import OrderProductCard from '../../views/OrderProductCard/OrderProductCard';
 import Button from '../../common/Button/Button';
@@ -9,12 +9,14 @@ import { API_URL } from '../../../config';
 
 const OrderForm = () => {
     const products = useSelector(getCart);
+    const dispatch = useDispatch()
 
     const [name, setName] = useState('');
     const [lastName, setLastName] = useState('');
     const [address, setAddress] = useState('');
     const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
+    const [status, setStatus] = useState(false);
 
     const totalPrice = () => {
         let price = 0;
@@ -24,7 +26,7 @@ const OrderForm = () => {
         return price;
     };
 
-    const orderHandler = (e) => {
+    const  orderHandler = async (e) => {
         e.preventDefault();
         console.log('order');
         const orderProducts = [];
@@ -53,61 +55,78 @@ const OrderForm = () => {
             body: JSON.stringify(order),
         };
 
-        fetch(`${API_URL}/orders`, option)
-        .then( res => {
+        try {
+            const res = await fetch(`${API_URL}/orders`, option);
             if (res.ok){
-                console.log('ok');
-                return res.json();
-            }else {
+                const data = await res.json();
+                setStatus(true);
+                dispatch(clearCart([]));
+                setName('');
+                setLastName('');
+                setAddress('');
+                setEmail('');
+                setPhone('');
+                console.log('Order placed successfully:', data);
+            } else {
                 console.log('error', res.status);
             }
-        });
-    };
+        } catch (error) {
+            console.log('Error', error)
+        } 
+    }
 
     return(
-        <div className={styles.order}>
-            <div className={styles.orderedProducts}>
-                <Row className={styles.list}>
-                    {
-                        products.map( product => 
-                            <OrderProductCard key={product.id} name={product.name} quantity={product.quantity} note={product.note} price={product.price} />
-                        )
-                    }
-                </Row>
-            </div>
-            <div className={styles.totalPrice}>
-                <span className={styles.totalPrice}>Total price: {totalPrice()} $</span>
-            </div>
-            <div className={styles.form}>
-                <form>
-                    <div className={styles.formGroup}>
-                        <label for='fname'>First name: </label>
-                        <input type='text' id='fname' name='name' value={name} onChange={e => setName(e.target.value)} />
-                    </div>
-                    <div className={styles.formGroup}>
-                        <label for='flastName'>Last name: </label>
-                        <input type='text' id='flastName' name='lastName' value={lastName} onChange={e => setLastName(e.target.value)}/>
-                    </div>
-                    <div className={styles.formGroup}>
-                        <label for='faddress'>Address: </label>
-                        <input type='text' id='faddress' name='address' value={address} onChange={e => setAddress(e.target.value)}/>
-                    </div>
-                    <div className={styles.formGroup}>
-                        <label for='fphone'>Phone: </label>
-                        <input type='tel' id='fpfone' name='phone' value={phone} onChange={e => setPhone(e.target.value)}/>
-                    </div>
-                    <div className={styles.formGroup}>
-                        <label for='femail'>Email: </label>
-                        <input type='email' id='femail' name='email' value={email} onChange={e => setEmail(e.target.value)}/>
-                    </div>
-                    
+        <div className={styles.orderForm}>
+            { status === false && 
+            <div className={styles.order}>
+                <div className={styles.orderedProducts}>
+                    <Row className={styles.list}>
+                        {
+                            products.map( product => 
+                                <OrderProductCard key={product.id} name={product.name} quantity={product.quantity} note={product.note} price={product.price} />
+                            )
+                        }
+                    </Row>
+                </div>
+                <div className={styles.totalPrice}>
+                    <span className={styles.totalPrice}>Total price: {totalPrice()} $</span>
+                </div>
+                <div className={styles.form}>
+                    <form>
+                        <div className={styles.formGroup}>
+                            <label for='fname'>First name: </label>
+                            <input type='text' id='fname' name='name' value={name} onChange={e => setName(e.target.value)} />
+                        </div>
+                        <div className={styles.formGroup}>
+                            <label for='flastName'>Last name: </label>
+                            <input type='text' id='flastName' name='lastName' value={lastName} onChange={e => setLastName(e.target.value)}/>
+                        </div>
+                        <div className={styles.formGroup}>
+                            <label for='faddress'>Address: </label>
+                            <input type='text' id='faddress' name='address' value={address} onChange={e => setAddress(e.target.value)}/>
+                        </div>
+                        <div className={styles.formGroup}>
+                            <label for='fphone'>Phone: </label>
+                            <input type='tel' id='fpfone' name='phone' value={phone} onChange={e => setPhone(e.target.value)}/>
+                        </div>
+                        <div className={styles.formGroup}>
+                            <label for='femail'>Email: </label>
+                            <input type='email' id='femail' name='email' value={email} onChange={e => setEmail(e.target.value)}/>
+                        </div>
+                        
 
-                    <div className={styles.btnContainer}>
-                        <Button action={orderHandler}>Order</Button>
-                    </div>
-                </form>
+                        <div className={styles.btnContainer}>
+                            <Button action={orderHandler}>Order</Button>
+                        </div>
+                    </form>
+                </div>
             </div>
+            }
             
+            { status === true && 
+                <div className={styles.orderComplete}>
+                    <span>Thanks for your order</span>
+                </div>}
         </div>
     );
 };
--- src/redux/cartRedux.js
+++ src/redux/cartRedux.js
@@ -11,6 +11,7 @@ const ADD_SAME_PRODUCT= createActionName('ADD_SAME_PRODUCT');
 const DELETE_PRODUCT = createActionName('DELETE_{PRODUCT');
 const UPDATE_UTY_PRODUCT = createActionName('UPDATE_UTY_PRODUCT');
 const UPDATE_NOTE_PRODUCT = createActionName('UPDATE_NOTE_PRODUCT');
+const CLEAR_CART = createActionName('CLEAR_CART');
 
 //action creators
 
@@ -19,6 +20,7 @@ export const addSameProduct = payload => ({type:ADD_SAME_PRODUCT, payload});
 export const deleteProduct = payload => ({type: DELETE_PRODUCT, payload});
 export const updateQtyProcut = payload => ({type: UPDATE_UTY_PRODUCT, payload});
 export const updateNoteProduct = payload => ({type: UPDATE_NOTE_PRODUCT, payload});
+export const clearCart = payload => ({type: CLEAR_CART, payload});
 
 const cartReducer = (statePart = [], action) => {
 
@@ -39,6 +41,8 @@ const cartReducer = (statePart = [], action) => {
             return statePart.map( product => (
                 product.id === action.payload.id ? {...product, note: action.payload.note}: product
             ));
+        case CLEAR_CART:
+            return [];
         default:
             return statePart;
     }
